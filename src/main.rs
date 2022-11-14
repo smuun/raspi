fbd56c7c1b8c54c8b0bcf9f58c03dd5ac452df5c
smuun <72632230+smuun@users.noreply.github.com>
2022-11-14 15:33:37 -0500

    Compile successfully

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 #![no_std]
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+use core::arch::asm;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    unsafe {
+        asm!("mov sp, #0x8000");
+    }
+    loop {}
 }
