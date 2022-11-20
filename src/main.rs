1b7a472311bedc8108d76a5e1c33cbb652c5e532
smuun <72632230+smuun@users.noreply.github.com>
2022-11-20 13:18:06 -0500

    Adapt hello world example from refs
    Specifically:
        - https://wiki.osdev.org/Raspberry_Pi_Bare_Bones_Rust
        - https://wiki.osdev.org/Raspberry_Pi_Bare_Bones
        - https://www.rpi4os.com/part1-bootstrapping/

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,72 @@
 
 use core::intrinsics::volatile_load;
 use core::intrinsics::volatile_store;
+use core::panic::PanicInfo;
+use core::arch::asm;
+use core::arch::global_asm;
 
 // raspi1 has peripheral base address 0x20000000
 // see refs for details
 const UART_DR: u32 = 0x20201000;
 const UART_FR: u32 = 0x20201018;
+
+// what would typically be in boot.S
+global_asm!(r#"
+// AArch32 mode
+ 
+// To keep this in the first portion of the binary.
+.section ".text.boot"
+ 
+// Make _start global.
+.globl _start
+ 
+        .org 0x8000
+// Entry point for the kernel.
+// r15 -> should begin execution at 0x8000.
+// r0 -> 0x00000000
+// r1 -> 0x00000C42 - machine id
+// r2 -> 0x00000100 - start of ATAGS
+// preserve these registers as argument for kernel_main
+_start:
+	// Setup the stack.
+	mov sp, #0x8000
+ 
+	// Clear out bss.
+	ldr r4, =__bss_start
+	ldr r9, =__bss_end
+	mov r5, #0
+	mov r6, #0
+	mov r7, #0
+	mov r8, #0
+	b       2f
+ 
+1:
+	// store multiple at r4.
+	stmia r4!, {{r5-r8}}
+ 
+	// If we are still below bss_end, loop.
+2:
+	cmp r4, r9
+	blo 1b
+ 
+	// Call kernel_main
+	ldr r3, =kernel_main
+	blx r3
+ 
+	// halt
+halt:
+	b halt
+"#);
+
+#[no_mangle]
+pub extern fn kernel_main() {
+    write("Hello Rust Kernel world!");
+    loop {
+        writec(getc())
+    }
+}
+
+
 
 fn mmio_write(reg: u32, val: u32) {
     unsafe { volatile_store(reg as *mut u32, val) }
@@ -42,10 +103,8 @@ fn write(msg: &str) {
     }
 }
 
-#[no_mangle]
-pub extern fn kernel_main() {
-    write("Hello Rust Kernel world!");
-    loop {
-        writec(getc())
-    }
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
