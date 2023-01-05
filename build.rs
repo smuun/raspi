adffb6302eed848b5544017a20e481db4616eeaf
smuun <72632230+smuun@users.noreply.github.com>
2023-01-05 12:04:08 +0100

    add example build.rs

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::{env, error::Error, fs::File, io::Write, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // build directory for this crate
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    // extend the library search path
+    println!("cargo:rustc-link-search={}", out_dir.display());
+
+    // put `linker.ld` in the build directory
+    File::create(out_dir.join("linker.ld"))?.write_all(include_bytes!("src/linker.ld"))?;
+
+    Ok(())
+}
