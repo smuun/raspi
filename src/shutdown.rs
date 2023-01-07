e51afd8342a92424303ed01a4cf958b9394a3c78
smuun <72632230+smuun@users.noreply.github.com>
2023-01-07 14:41:38 +0100

    trick no longer appears to be needed

--- src/shutdown.rs
+++ src/shutdown.rs
@@ -15,36 +15,18 @@ pub enum QemuExitCode {
 /// Use QEMU angel mode (-semihosting must be enabled)
 /// to exit.  Use for testing.
 pub fn qemu_angel_exit(code: QemuExitCode) {
-    fool_rustc();
+    // fool_rustc();
+    const EXIT: u32 = 0x18;
+    const OK: u32 = 0x20026;
+    const FAIL: u32 = 0x20027;
+
     match code {
         QemuExitCode::Ok => unsafe {
-            asm!(
-                "
-                mov r1, #0x20000
-                add r1, r1, #0x00026
-                mov r0, #0x18
-                svc 0x00123456
-                "
-            );
+            // r[0-3] -> a[1-4]
+            asm!("svc 0x00123456", in("r0") EXIT, in("r1") OK);
         },
         QemuExitCode::Fail => unsafe {
-            asm!(
-                "
-                mov r1, #0x20000
-                add r1, r1, #0x00027
-                mov r0, #0x18
-                svc 0x00123456
-                "
-            );
+            asm!("svc 0x00123456", in("r0") EXIT, in("r1") FAIL);
         },
-    }
-}
-
-// for some reason this is necessary not to optimize away the entire
-// setup.s????
-pub fn fool_rustc() {
-    unsafe {
-        asm!("ldr r0, =_reset");
-        asm!("ldr r0, =_ivt");
     }
 }
